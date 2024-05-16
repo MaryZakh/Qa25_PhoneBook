bb3601e33cc460e5623818a9007839392cc2bba8
Maria Z <z0559882272@gmail.com>
2024-05-16 18:57:01 +0300

    registration tests

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition() {
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+    }
+
+    @Test
+    public void registrationSuccess() {
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
+        User user = new User().withEmail("dom"+i+"@gmail.com").withPassword("Ddom123456$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+        Assert.assertTrue(app.getHelperUser().isNoContactsHereDisplayed());
+    }
+
+    @Test(description = "Bug report #12569, Fixed")
+    public void registrationWrongEmail() {
+        User user = new User().withEmail("domgmail.com").withPassword("Ddom123456$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+    @Test
+    public void registrationWrongPassword() {
+        User user = new User().withEmail("dom@gmail.com").withPassword("Ddom123");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+
+    @Test
+    public void registrationExistUser() {
+        User user = new User().withEmail("mara@gmail.com").withPassword("Mmar123456$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("User already exist"));
+    }
+
+
+}
